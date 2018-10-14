8fe3a11f7cf4f56e47e0f6d07afd91a84735b5c1
kohlicekjan <kohlicekjan@gmail.com>
2018-10-14 19:51:34 +0200

    Change directory for end-to-end tests

--- jest.config.js
+++ jest.config.js
@@ -141,7 +141,7 @@ module.exports = {
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
   testPathIgnorePatterns: [
     "/node_modules/",
-    "/cypress/"
+    "/test/integration/"
   ],
 
   // The regexp pattern Jest uses to detect test files
